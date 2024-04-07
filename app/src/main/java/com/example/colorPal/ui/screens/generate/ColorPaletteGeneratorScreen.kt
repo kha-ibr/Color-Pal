898af407ba63d4ee1d0108dc00ab6f02073bb815
Khalid Ibrahim <64450080+khaibr20@users.noreply.github.com>
2024-04-07 20:00:25 +0200

    Refine the color card ui

--- app/src/main/java/com/example/colorPal/ui/screens/generate/ColorPaletteGeneratorScreen.kt
+++ app/src/main/java/com/example/colorPal/ui/screens/generate/ColorPaletteGeneratorScreen.kt
@@ -4,16 +4,15 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Lock
@@ -29,6 +28,7 @@ import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material.icons.rounded.Tune
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -48,7 +48,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.tooling.preview.Preview
@@ -215,10 +214,10 @@ fun ColorPaletteGeneratorScreen(
         Column(
             modifier = Modifier
                 .padding(it)
-                .fillMaxSize()
+                .padding(horizontal = 16.dp),
         ) {
             viewModel.colorStates.forEachIndexed { index, colorState ->
-                ColorPaletteCard(
+                ColorCard(
                     modifier = Modifier.weight(1f),
                     colorCardState = colorState,
                     onCardClick = {
@@ -228,41 +227,35 @@ fun ColorPaletteGeneratorScreen(
                     },
                     onCardLock = { clickedId -> viewModel.toggleLock(clickedId) }
                 )
+
                 // Remove spacing on the last card
                 if (index <= viewModel.colorStates.size - 1)
                     Spacer(modifier = Modifier.height(10.dp))
             }
 
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = padding, end = padding)
-                    .weight(1f),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.height(100.dp), verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(
-                    onClick = {
-                        viewModel.colorStates.forEach { colorState ->
-                            if (!colorState.isLocked) {
-                                val newColor = Color(
-                                    (0..255).random(),
-                                    (0..255).random(),
-                                    (0..255).random()
-                                )
+                Button(onClick = {
+                    viewModel.colorStates.forEach { colorState ->
+                        if (!colorState.isLocked) {
+                            val newColor = Color(
+                                (0..255).random(),
+                                (0..255).random(),
+                                (0..255).random()
+                            )
 
-                                viewModel.updateColor(colorState.id, newColor)
-                            }
+                            viewModel.updateColor(colorState.id, newColor)
                         }
-                    }, modifier = Modifier
-                        .fillMaxWidth(.9f)
-                        .padding(end = 10.dp)
-                ) {
+                    }
+                }, modifier = Modifier.fillMaxWidth(.85f)) {
                     Text(text = "Generate")
                 }
 
-                IconButton(onClick = { isMoreOptionSheetVisible = true }) {
-                    Icon(imageVector = Icons.Rounded.Tune, contentDescription = "Tune")
+                Box(modifier = Modifier.fillMaxWidth(), Alignment.CenterEnd) {
+                    IconButton(onClick = { isMoreOptionSheetVisible = true }) {
+                        Icon(imageVector = Icons.Rounded.Tune, contentDescription = "More Option")
+                    }
                 }
             }
         }
@@ -271,40 +264,51 @@ fun ColorPaletteGeneratorScreen(
 }
 
 @Composable
-fun ColorPaletteCard(
+fun ColorCard(
     modifier: Modifier = Modifier,
-    cardShape: Shape = RoundedCornerShape(8.dp),
+    textColor: Color = Color.Black,
     colorCardState: ColorCardState,
     onCardClick: () -> Unit,
     onCardLock: (Int) -> Unit
 ) {
     val padding = 16.dp
 
-    Row(
+    Card(
         modifier = modifier
             .fillMaxWidth()
-            .padding(start = padding, end = padding)
-            .clickable { onCardClick() }
-            .background(colorCardState.color, shape = cardShape)
-            .padding(padding),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Text(
-            text = colorCardState.text,
-            style = MaterialTheme.typography.bodyLarge,
-            color = Color.White,
-        )
-
-        IconButton(onClick = { onCardLock(colorCardState.id) }) {
-            val icon = if (colorCardState.isLocked) Icons.Default.Lock else Icons.Default.LockOpen
-
-            Icon(
-                imageVector = icon,
-                contentDescription = if (colorCardState.isLocked) "Unlock Palette" else "Lock Palette",
-                tint = Color(255, 255, 255).copy(alpha = .6f),
-                modifier = Modifier.size(24.dp)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clickable { onCardClick() }
+                .background(colorCardState.color),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                colorCardState.text,
+                modifier = Modifier
+                    .weight(5f)
+                    .padding(start = padding),
+                style = MaterialTheme.typography.bodyLarge,
+                color = textColor
             )
+
+            Box(
+                modifier = modifier
+                .fillMaxHeight()
+                .width(50.dp)
+                .clickable { onCardLock(colorCardState.id) },
+                Alignment.CenterEnd
+            ) {
+                val icon = if (colorCardState.isLocked) Icons.Default.Lock else Icons.Default.LockOpen
+
+                Icon(
+                    imageVector = icon,
+                    contentDescription = if (colorCardState.isLocked) "Unlock Palette" else "Lock Palette",
+                    modifier = modifier.padding(end = padding)
+                )
+            }
         }
     }
 
